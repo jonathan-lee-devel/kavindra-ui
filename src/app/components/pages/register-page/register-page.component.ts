faa4ae133bc2529b7cac5eec42b174f72b686599
Jonathan Lee <jonathan.lee.devel@gmail.com>
2024-10-22 06:22:05 +0100

    Add email registration functionality
    Integrated email and password fields with user authentication store. Added method to handle email sign-up in the registration component and updated corresponding template bindings.

--- src/app/components/pages/register-page/register-page.component.ts
+++ src/app/components/pages/register-page/register-page.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { UserAuthenticationStore } from '../../../+state/auth/user-auth.store';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register-page',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './register-page.component.html',
   styleUrl: './register-page.component.scss',
 })
-export class RegisterPageComponent {}
+export class RegisterPageComponent {
+  protected readonly userAuthenticationStore = inject(UserAuthenticationStore);
+  email: string = '';
+  password: string = '';
+
+  doRegisterWithEmail() {
+    this.userAuthenticationStore.attemptSupabaseSignUpWithEmail(
+      this.email,
+      this.password,
+    );
+  }
+}
