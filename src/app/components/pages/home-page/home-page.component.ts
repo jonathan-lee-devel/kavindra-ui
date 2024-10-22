d6393eb0502631c7ce31689603b951203b323923
Jonathan Lee <jonathan.lee.devel@gmail.com>
2024-10-22 01:19:01 +0100

    Add project and team member management UI
    Replaced cashflow and recent activity sections with UI for managing projects and team members. New sections include buttons for creating projects and sending team member invites, along with a recommended team members list.

--- src/app/components/pages/home-page/home-page.component.ts
+++ src/app/components/pages/home-page/home-page.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+    imports: [
+        NgIf
+    ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
 export class HomePageComponent {
-
+  projects: unknown[] = [];
 }
